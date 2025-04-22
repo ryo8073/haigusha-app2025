c70bc7d5b0865cbd90779082ea8d0eef9f6f4225
Ryo Yamanami <yamanami-ryo@heya.co.jp>
2025-04-22 17:16:25 +0900

    Add Google Analytics and improve UI design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,38 @@
 // app/layout.tsx
-import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
 
-export const metadata: Metadata = {
-  title: '配偶者居住権評価計算',
-  description: '配偶者居住権の評価額を計算するアプリケーション',
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: '配偶者居住権評価計算アプリ',
+  description: '配偶者居住権の評価額を計算するためのアプリケーション',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="ja" suppressHydrationWarning>
-      <body className="bg-gray-50" suppressHydrationWarning>
-        {children}
-      </body>
+    <html lang="ja">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-BEQ7SYMQJ3"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-BEQ7SYMQJ3');
+          `}
+        </Script>
+      </head>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
